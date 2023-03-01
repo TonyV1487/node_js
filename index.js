14a7adb922ac7d13007e5a7450ead87264e82571
Tony Vester <tvester1487@gmail.com>
2023-02-28 21:31:45 -0600

    Init

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile('./files/starter.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})
+
+console.log('Hello...')
+
+// exit on uncaught errors
+process.on('uncaughtException', err => {
+    console.error(`There was an uncaught error: ${err}`);
+    process.exit(1);
+})
