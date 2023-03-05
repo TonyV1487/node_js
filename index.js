5399fcf438d8a018be2acb291968e890ce0843ab
Tony Vester <tvester1487@gmail.com>
2023-03-04 22:19:04 -0600

    Callback Hell

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.readFile('./files/starter.txt', 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname, 'files', 'starter.txt' ), 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 })
 
 console.log('Hello...')
+
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt' ), 'Nice to meet you.', (err) => {
+    if (err) throw err;
+    console.log('Write complete');
+
+    fs.appendFile(path.join(__dirname, 'files', 'reply.txt' ), '\n\n Yes it is', (err) => {
+        if (err) throw err;
+        console.log('Append complete');
+
+        fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newRepyly.txt'), (err) => {
+            if (err) throw err;
+            console.log('Rename complete');
+        })
+    })
+})
+
+
 
 // exit on uncaught errors
 process.on('uncaughtException', err => {
